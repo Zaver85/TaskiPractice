a0b5bfbeb108f98ea8c96f78a0100ac4b8425231
Zaver85 <Zaver85.com@mail.ru>
2024-11-19 13:23:51 +0700

    created Task9

---
+++ src/taski/Task9.java
@@ -0,0 +1,27 @@
+/*
+Реализуй метод main(String[]), который выводит массивы strings и ints в консоли с помощью метода Arrays.toString().
+
+Требования:
+        •	В классе должна быть публичная статическая переменная strings типа String[].
+        •	В классе должна быть публичная статическая переменная ints типа int[].
+        •	В методе main(String[]) выведи в консоли массив strings с помощью метода Arrays.toString(Object[]).
+        •	В методе main(String[]) выведи в консоли массив ints с помощью метода Arrays.toString(int[]).*/
+
+package taski;
+
+import java.util.Arrays;
+
+public class Task9 {
+
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        String str = Arrays.toString(strings);
+        System.out.println(str);
+
+        String str2 = Arrays.toString(ints);
+        System.out.println(str2);
+        //напишите тут ваш код
+    }
+}
