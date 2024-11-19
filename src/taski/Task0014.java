8dfc112b75a16565be3d26a828e6bcc6b7a70345
Zaver85 <Zaver85.com@mail.ru>
2024-11-20 00:42:15 +0700

    created Task0014, Task0015

---
+++ src/taski/Task0014.java
@@ -0,0 +1,18 @@
+/*Реализуй метод main(String[]), который сортирует массив array по возрастанию.
+Для сортировки массива используй метод Arrays.sort(int[]).
+
+Требования:
+        •	Реализуй метод main(String[]) согласно условию.*/
+
+package taski;
+
+import java.util.Arrays;
+
+public class Task0014 {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        Arrays.sort(array);//напишите тут ваш код
+        System.out.println(Arrays.toString(array));
+    }
+}
