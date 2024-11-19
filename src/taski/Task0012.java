39b9fe96fac93666aa479b1ce847b9271afa2894
Zaver85 <Zaver85.com@mail.ru>
2024-11-19 23:04:48 +0700

    created Task0013

--- src/taski/Task0012.java
+++ src/taski/Task0012.java
@@ -17,19 +17,16 @@ public class Task0012 {
     public static int valueEnd = 13;
 
     public static void main(String[] args) {
-        int size = array.length / 2;
 
         if (array.length % 2 == 0) {
             for (int i = 0; i < array.length / 2; i++) {
                 Arrays.fill(array, 0, array.length / 2, valueStart);
-                Arrays.fill(array, size, array.length, valueEnd);
+                Arrays.fill(array, array.length / 2, array.length, valueEnd);
             }
-        }else {
-            Arrays.fill(array, 0, size + 1, valueStart);
-            Arrays.fill(array, size + 1, array.length, valueEnd);
+        } else {
+            Arrays.fill(array, 0, ((array.length / 2) + 1), valueStart);
+            Arrays.fill(array, ((array.length / 2) + 1), array.length, valueEnd);
         }
-
-        //напишите тут ваш код
         System.out.println(Arrays.toString(array));
     }
 }
