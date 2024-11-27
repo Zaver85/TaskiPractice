3cc40754a18b741ae03ac9ef6b0d682127d61dc3
Zaver85 <Zaver85.com@mail.ru>
2024-11-27 18:54:22 +0700

    created Task0053

---
+++ src/taskiFrom0041To0060/Task0053.java
@@ -0,0 +1,23 @@
+/*В классе замени все полные имена классов в коде на короткие.
+
+Требования:
+        •	В методе main должны быть только короткие имена классов: остальное менять нельзя.
+•	В классе должно быть 3 импорта.*/
+
+package taskiFrom0041To0060;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/*
+Импорты: Part 2
+*/
+
+public class Task0053 {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
