a5848df45133b8233508afd96009d463795a0717
Zaver85 <Zaver85.com@mail.ru>
2024-11-27 18:50:43 +0700

    created Task0052

---
+++ src/taskiFrom0041To0060/Task0052.java
@@ -0,0 +1,19 @@
+/*В классе замени все короткие имена классов в коде на полные.
+
+Требования:
+•	В методе main должны быть только полные имена классов: остальное менять нельзя.
+•	В классе Solution не должно быть импортов.*/
+
+package taskiFrom0041To0060;
+
+/*
+Импорты: Part 1
+*/
+
+public class Task0052 {
+    public static void main(String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
