ba5c69e3f6ef0042612bec2770e5186dc3b1ad47
Zaver85 <Zaver85.com@mail.ru>
2024-11-27 21:50:52 +0700

    created Task0058

---
+++ src/taskiFrom0041To0060/task0058/Pair.java
@@ -0,0 +1,22 @@
+package taskiFrom0041To0060.task0058;
+
+public class Pair {
+    private int x;
+    private int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("x=%d, y=%d", x, y);
+    }
+
+    public void swap() {
+        x ^= y;
+        y ^= x;
+        x ^= y;
+    }
+}
