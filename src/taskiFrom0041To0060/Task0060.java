fc5416915f056000758026fc5e3891d2f800426a
Zaver85 <Zaver85.com@mail.ru>
2024-11-27 23:22:36 +0700

    created Task0060

---
+++ src/taskiFrom0041To0060/Task0060.java
@@ -0,0 +1,30 @@
+/*Реализуй методы:
+
+setFlag(int number, int flagPos) — устанавливает значение "1" биту под индексом flagPos
+числа number и возвращает новое значение.
+resetFlag(int number, int flagPos) — устанавливает значение "0" биту под индексом flagPos
+числа number и возвращает новое значение.
+checkFlag(int number, int flagPos) — проверяет значение бита под индексом flagPos
+числа number и возвращает true, если значение "1" и false, если "0".
+Требования:
+        •	Реализуй метод setFlag(int, int) согласно условию.
+        •	Реализуй метод resetFlag(int, int) согласно условию.
+        •	Реализуй метод checkFlag(int, int) согласно условию.*/
+
+package taskiFrom0041To0060;
+
+public class Task0060 {
+    public static int setFlag(int number, int flagPos) {
+        number = number | (1 << flagPos);//напишите тут ваш код
+        return number;
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        number = number & ~(1 << flagPos);//напишите тут ваш код
+        return number;
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        return (number & (1 << flagPos)) == (1 << flagPos);
+    }
+}
