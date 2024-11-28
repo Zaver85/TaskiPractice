6ffb7a30e1928e38b945f5bb0833cf1d89ced032
Zaver85 <Zaver85.com@mail.ru>
2024-11-28 21:43:58 +0700

    created Task0072

---
+++ src/taskiFrom0061To0080/Task0072.java
@@ -0,0 +1,25 @@
+/*Выведи на экран следующий текст в две строки:
+It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
+It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"
+
+Подсказка:
+        \” – экранирование двойной кавычки;
+\\ – экранирование обратной косой черты (\).
+
+
+Больше про экранирование символов и Escape-последовательности в Java читай в статье "Экранирование символов в Java".
+
+Требования:
+•	Нужно, чтобы программа выводила текст.
+•	Нужно, чтобы было выведено две строки.
+•	Текст первой строки должен быть: It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
+•	Текст второй строки должен быть: It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"*/
+
+package taskiFrom0061To0080;
+
+public class Task0072 {
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
+}
