9604f3fb05362aa3628a54896beacc64ca529f23
Zaver85 <Zaver85.com@mail.ru>
2024-11-28 23:14:30 +0700

    created Task0073

--- src/taskiFrom0061To0080/Task0072.java
+++ src/taskiFrom0061To0080/Task0072.java
@@ -3,17 +3,15 @@ It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
 It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"
 
 Подсказка:
-        \” – экранирование двойной кавычки;
+\” – экранирование двойной кавычки;
 \\ – экранирование обратной косой черты (\).
-
-
-Больше про экранирование символов и Escape-последовательности в Java читай в статье "Экранирование символов в Java".
 
 Требования:
 •	Нужно, чтобы программа выводила текст.
 •	Нужно, чтобы было выведено две строки.
 •	Текст первой строки должен быть: It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
-•	Текст второй строки должен быть: It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"*/
+•	Текст второй строки должен быть: It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"
+*/
 
 package taskiFrom0061To0080;
 
