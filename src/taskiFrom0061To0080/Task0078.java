33f09e1a4b27cd7d6cd97fe8bb192d397a81320b
Zaver85 <Zaver85.com@mail.ru>
2024-12-01 19:08:27 +0700

    created Task0078

---
+++ src/taskiFrom0061To0080/Task0078.java
@@ -0,0 +1,36 @@
+/*Используя StringTokenizer раздели query на части по разделителю delimiter.
+
+    Пример:
+getTokens("java.util.stream", "\\.")
+возвращает массив строк
+{"java", "util", "stream"}
+
+Hint: решить задачу поможет метод countTokens().
+
+Требования:
+•	Нужно, чтобы метод getTokens(String, String) использовал StringTokenizer.
+•	Нужно, чтобы метод getTokens(String, String) возвращал массив типа String,
+        заполненный согласно условию задачи.*/
+
+package taskiFrom0061To0080;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Task0078 {
+    public static void main(String[] args) {
+        String packagePath = "java.util.stream";
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer stringTokenizer = new StringTokenizer(query, delimiter);
+        String[] result = new String[stringTokenizer.countTokens()];
+        int i = 0;
+        while (stringTokenizer.hasMoreTokens()) {
+            result[i++] = stringTokenizer.nextToken();
+        }
+        return result;
+    }
+}
