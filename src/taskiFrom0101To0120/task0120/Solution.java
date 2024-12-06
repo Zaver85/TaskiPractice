5bb82174e846be4b112790ba944e3b58cf8a4166
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 23:13:08 +0700

    created Task0120

---
+++ src/taskiFrom0101To0120/task0120/Solution.java
@@ -0,0 +1,12 @@
+/*Исправь ошибку в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	Программа должна компилироваться и работать.*/
+
+package taskiFrom0101To0120.task0120;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("TestPackage");
+    }
+}
