5415eddd3b270aee7045e7df9c7e2742d7916c73
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 23:16:19 +0700

    created Task0121

---
+++ src/taskiFrom0121To0140/task0121/Solution.java
@@ -0,0 +1,17 @@
+/*Исправь ошибку в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	Программа должна компилироваться и работать.*/
+
+package taskiFrom0121To0140.task0121;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
