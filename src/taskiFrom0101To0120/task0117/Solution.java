694c3373a84b7a0091f4932969451e303d4ae7af
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 23:00:07 +0700

    created Task0117

---
+++ src/taskiFrom0101To0120/task0117/Solution.java
@@ -0,0 +1,18 @@
+/*Кажется, в этот раз проблема снова со скобками. Но уже с другими.
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	В методе main класса Solution нужно исправить ошибки компиляции.*/
+
+package taskiFrom0101To0120.task0117;
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
