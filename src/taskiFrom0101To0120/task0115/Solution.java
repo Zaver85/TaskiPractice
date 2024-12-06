324522054c7fc778057a89e333bb36f2f3063c34
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 22:55:24 +0700

    created Task0115

---
+++ src/taskiFrom0101To0120/task0115/Solution.java
@@ -0,0 +1,16 @@
+/*"Кон", "котэ" и "нация". Как соединить эти три строки?
+Ты же помнишь, как соединять String'и в Java?
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	В методе main класса Solution нужно исправить ошибки: при выводе на экран должна использоваться конкатенация строк.*/
+
+package taskiFrom0101To0120.task0115;
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
