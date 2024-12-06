88c59fa9215e395518930b5a75158add8156686a
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 23:03:41 +0700

    created Task0118

---
+++ src/taskiFrom0101To0120/task0118/Solution.java
@@ -0,0 +1,12 @@
+/*Сделай одно исправление в коде, чтобы программа запускалась.
+
+Требования:
+•	В методе main класса Solution нужно сделать одно исправление, чтобы программа заработала.*/
+
+package taskiFrom0101To0120.task0118;
+
+public class Solution {
+    public static void main(String... errors) {
+        System.out.println("It works!");
+    }
+}
