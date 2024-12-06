ca8aba50483b0aeb42ddd036698b21d13309db79
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 22:50:35 +0700

    created Task0113

---
+++ src/taskiFrom0101To0120/task0113/Solution.java
@@ -0,0 +1,14 @@
+/*Точки и запятые важны не только в пунктуации в обычном тексте. Исправь ошибки в коде,
+чтобы программа компилировалась и работала.
+
+Требования:
+•	В методе main класса Solution должны быть исправлены ошибки компиляции.*/
+
+package taskiFrom0101To0120.task0113;
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
