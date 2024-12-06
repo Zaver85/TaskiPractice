350aef86b9dc969ed49130cb2b2b5b7a0ff8f8fd
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 19:57:00 +0700

    created Task0111

---
+++ src/taskiFrom0101To0120/task0111/Solution.java
@@ -0,0 +1,43 @@
+/*Давай проведем рефакторинг кода: вынесем отдельно утильные методы.
+Для этого создай внутренний статический (вложенный) класс Calculator и
+перенеси в него методы add, subtract, multiply и divide.
+Не забудь подкорректировать их вызовы.
+
+Требования:
+•	В классе Solution нужно создать вложенный (static nested) класс Calculator.
+•	Методы add, subtract, multiply и divide нужно перенести в класс Calculator.
+•	В методе main должны быть вызваны методы add, subtract, multiply и divide класса Calculator.*/
+
+package taskiFrom0101To0120.task0111;
+
+public class Solution {
+    public static final String EQUAL = " = ";
+
+    static class Calculator {
+
+        public static void add(int a, int b) {
+            System.out.println(a + " + " + b + EQUAL + (a + b));
+        }
+
+        public static void subtract(int a, int b) {
+            System.out.println(a + " - " + b + EQUAL + (a - b));
+        }
+
+        public static void multiply(int a, int b) {
+            System.out.println(a + " * " + b + EQUAL + (a * b));
+        }
+
+        public static void divide(int a, int b) {
+            System.out.println(a + " / " + b + EQUAL + (a / b));
+        }
+    }
+
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 15;
+        Calculator.add(a, b);
+        Calculator.subtract(a, b);
+        Calculator.multiply(a, b);
+        Calculator.divide(a, b);
+    }
+}
