8826bc3c3c55ff28d865ce7e85d793a9f70247cb
Zaver85 <Zaver85.com@mail.ru>
2024-12-06 22:52:44 +0700

    created Task0114

---
+++ src/taskiFrom0101To0120/task0114/Solution.java
@@ -0,0 +1,14 @@
+/*В коде уже есть кавычки, но, как оказалось, их недостаточно.
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	В методе main класса Solution должны быть исправлены ошибки компиляции.*/
+
+package taskiFrom0101To0120.task0114;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
