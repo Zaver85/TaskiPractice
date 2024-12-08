5d1851e2d75ff3ad81ab7b4865f7a71301cf15b9
Zaver85 <Zaver85.com@mail.ru>
2024-12-08 17:24:37 +0700

    created Task0140

---
+++ src/taskiFrom0121To0140/task0140/Solution.java
@@ -0,0 +1,48 @@
+/*В классе Solution есть список сотрудников waitingEmployees, которые ждут зарплату,
+и список сотрудников alreadyGotSalaryEmployees, которые её уже получили.
+Необходимо реализовать метод paySalary(String), который в качестве аргумента
+принимает имя сотрудника, желающего получить зарплату.
+Метод должен проверить наличие сотрудника в списке waitingEmployees,
+и если он не получал зарплату, выдать её:
+
+Если параметр метода paySalary(String) равняется null, ничего не нужно делать.
+Добавить имя сотрудника в список alreadyGotSalaryEmployees.
+В списке waitingEmployees заменить имя сотрудника на null.
+Требования:
+•	Метод paySalary(String) должен добавлять в список alreadyGotSalaryEmployees имя сотрудника,
+если он есть в списке waitingEmployees.
+•	Метод paySalary(String) должен заменять имя сотрудника на значение null,
+если он есть в списке waitingEmployees.
+•	Метод paySalary(String) не должен изменять списки waitingEmployees и alreadyGotSalaryEmployees,
+если переданного имени нет в списке waitingEmployees.
+•	Если в метод paySalary(String) передается null, метод не должен добавлять это значение
+в waitingEmployees или alreadyGotSalaryEmployees.*/
+
+package taskiFrom0121To0140.task0140;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static ArrayList<String> waitingEmployees = new ArrayList<>();
+    public static ArrayList<String> alreadyGotSalaryEmployees = new ArrayList<>();
+
+    public static void initEmployees() {
+        waitingEmployees.add("Гвинно");
+        waitingEmployees.add("Гунигерд");
+        waitingEmployees.add("Боргелейф");
+        waitingEmployees.add("Нифрод");
+        waitingEmployees.add("Альбиуф");
+        waitingEmployees.add("Иногрим");
+        waitingEmployees.add("Фриле");
+    }
+
+    public static void main(String[] args) {
+        initEmployees();
+    }
+
+    public static void paySalary(String name) {
+        if (waitingEmployees.contains(name)) {
+            alreadyGotSalaryEmployees.set(waitingEmployees.indexOf(name), null);
+        }
+    }
+}
