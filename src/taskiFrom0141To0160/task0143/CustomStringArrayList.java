e0df953f6e605f6137ed54d674c7cda3fa397ce2
Zaver85 <Zaver85.com@mail.ru>
2024-12-09 13:19:36 +0700

    created Task0143

---
+++ src/taskiFrom0141To0160/task0143/CustomStringArrayList.java
@@ -0,0 +1,32 @@
+package taskiFrom0141To0160.task0143;
+
+public class CustomStringArrayList {
+
+    private int size;
+    private int capacity;
+    private String[] elements;
+
+    public CustomStringArrayList() {
+        capacity = 10;
+        size = 0;
+        elements = new String[capacity];
+    }
+
+    public void add(String element) {
+        if (size == capacity) {
+            grow();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    private void grow() {
+        capacity = (int) (capacity * 1.5);
+        String[] newElements = new String[capacity];
+        for (int i = 0; i < elements.length; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+    }
+
+}
