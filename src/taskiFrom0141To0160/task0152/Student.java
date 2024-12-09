2d60131b6c47ba11ec3f9dcf4a05d45289a39829
Zaver85 <Zaver85.com@mail.ru>
2024-12-09 20:56:32 +0700

    created Task0152

---
+++ src/taskiFrom0141To0160/task0152/Student.java
@@ -0,0 +1,13 @@
+package taskiFrom0141To0160.task0152;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/taskiFrom0141To0160/task0152/UniversityDepartment.java
@@ -0,0 +1,30 @@
+/*В классе UniversityDepartment есть метод printStudentNames(ArrayList),
+который принимает список объектов и выводит их имена. Но на данный момент программа не компилируется.
+Твоя задача исправить этот баг. Для этого нужно исправить сигнатуру метода printStudentNames(ArrayList),
+чтобы он принимал список объектов только типа Student(используй дженерик).
+Метод main не участвует в проверке.
+
+Требования:
+•	Исправь сигнатуру метода printStudentNames, чтобы он принимал список только студентов.
+•	Реализацию метода printStudentNames не изменять.*/
+
+package taskiFrom0141To0160.task0152;
+
+import java.util.ArrayList;
+
+public class UniversityDepartment {
+
+    public static void main(String[] args) {
+        var students = new ArrayList<Student>();
+        students.add(new Student("Акакий"));
+        students.add(new Student("Любослав"));
+
+        printStudentNames(students);
+    }
+
+    public static void printStudentNames(ArrayList<Student> students) {
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getName());
+        }
+    }
+}
