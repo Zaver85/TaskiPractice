2938a9263f5d925e8246d144b55c7424f6f713c8
Zaver85 <Zaver85.com@mail.ru>
2024-12-13 18:08:19 +0700

    created aTaskNotFromTheCollection 0004

--- src/aTaskNotFromTheCollection/task0003/Main.java
+++ src/aTaskNotFromTheCollection/task0003/Main.java
@@ -1,7 +1,12 @@
+/*Реализовать консольное приложение, которое:
+1. Принимает от пользователя и “запоминает” строки.
+2. Если введено print, выводит строки так, чтобы последняя введенная
+была первой в списке, а первая - последней.
+3. Если введено revert, удаляет предыдущую введенную строку из памяти.*/
+
 package aTaskNotFromTheCollection.task0003;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
