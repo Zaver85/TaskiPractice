e4f9d228f099634fff1255beb7b0e122aa2cd383
Zaver85 <Zaver85.com@mail.ru>
2024-12-15 16:30:59 +0700

    created Task0185

---
+++ src/taskiFrom0181To0200/task0185/Solution.java
@@ -0,0 +1,39 @@
+/*В методе main перехвати RuntimeException. Определи, какое исключение в нем сохранено.
+Если это FileNotFoundException, выведи в консоли сообщение: "Не удалось прочесть файл.",
+а если FileSystemException — "Не удалось записать в файл.".
+
+Требования:
+•	В методе main должно перехватываться RuntimeException.
+•	Нужно вывести "Не удалось прочесть файл.", если в RuntimeException упаковано FileNotFoundException.
+•	Нужно вывести "Не удалось записать в файл.", если в RuntimeException упаковано FileSystemException.
+•	Класс FileManager не изменя*/
+
+package taskiFrom0181To0200.task0185;
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
+
+/*
+Распаковка исключений
+*/
+
+public class Solution {
+
+    public static final String FAILED_TO_READ = "Не удалось прочесть файл.";
+    public static final String FAILED_TO_WRITE = "Не удалось записать в файл.";
+
+    public static FileManager fileManager = new FileManager();
+
+    public static void main(String[] args) {
+        try {
+            fileManager.copyFile("book.txt", "book_final_copy.txt");
+            fileManager.copyFile("book_final_copy.txt", "book_last_copy.txt");
+        } catch (RuntimeException e) {
+            if (e.getCause() instanceof FileNotFoundException) {
+                System.out.println(FAILED_TO_READ);
+            } else if (e.getCause() instanceof FileSystemException) {
+                System.out.println(FAILED_TO_WRITE);
+            }
+        }
+    }
+}
