fe58db8908bece65858aa1881cde2a88b287c0bf
Zaver85 <Zaver85.com@mail.ru>
2025-01-30 22:17:31 +0700

    created Task0229

---
+++ src/taskiFrom0221To0240/task0229/Solution.java
@@ -0,0 +1,38 @@
+/*Что общего у всех временных зон и Пекина?
+Правильно! Они позволят нам потренироваться в использовании классов ZoneId и ZonedDateTime :)
+
+Реализуй два метода:
+
+в методе getSortedZones верни множество TreeSet всех временных зон;
+в методе getBeijingTime верни текущую дату и время в Пекине (временная зона для него — "Asia/Shanghai").
+Требования:
+Метод getSortedZones должен вернуть множество всех временных зон.
+Метод getBeijingDateTime должен вернуть текущую дату и время в Пекине.*/
+
+package taskiFrom0221To0240.task0229;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.TreeSet;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        TreeSet<String> sortedZones = getSortedZones();
+        System.out.println(sortedZones.size());
+        System.out.println(sortedZones.first());
+        System.out.println(sortedZones.last());
+
+        System.out.println(getBeijingDateTime());
+    }
+
+    static TreeSet<String> getSortedZones() {
+        return new TreeSet<>(ZoneId.getAvailableZoneIds());
+    }
+
+    static ZonedDateTime getBeijingDateTime() {
+        ZoneId zone = ZoneId.of("Asia/Shanghai");
+        return ZonedDateTime.now(zone);
+    }
+}
